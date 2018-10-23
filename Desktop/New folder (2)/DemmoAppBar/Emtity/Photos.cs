e519efccf05f86d9d23a3f0ef5a7d3d4c0a4c689
LeVien <vienlvv14091998@gmail.com>
2018-10-23 10:38:38 +0700

    first commit

---
+++ Desktop/New folder (2)/DemmoAppBar/Emtity/Photos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemmoAppBar.Emtity
+{
+    class Photos
+    {
+        private string _fileName;
+        private string _filePath;
+
+        public string fileName { get => _fileName; set => _fileName = value; }
+        public string filePath { get => _filePath; set => _filePath = value; }
+    }
+}
---
+++ Desktop/New folder (2)/DemmoAppBar/Views/PhotoGellery.xaml.cs
@@ -0,0 +1,47 @@
+﻿using DemmoAppBar.Emtity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DemmoAppBar.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PhotoGellery : Page
+    {
+        private ObservableCollection<Photos> listPhoto;
+        public PhotoGellery()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ChooseImage(object sender, RoutedEventArgs e)
+        {
+            
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await openPicker.PickSingleFileAsync();
+        }
+    }
+}
